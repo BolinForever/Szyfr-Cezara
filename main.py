2f91c769e72b958edfd477d0056170c85be01c01
cos <uczen.ckziu@jaworzno.pl>
2022-12-09 13:53:25 +0100

    final

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-word = "kaszanka"
+class Hypothesis4:
+    def encrypt(self, word):
+        print("Word you want to encrypt: ", word)
+        new_word = ""
+        for i in range(len(word)):
+            encode = (ord(word[i]) + 1)
+
+            if encode > 122:
+                encode -= 26
+
+            new_word += chr(encode)
+
+        return new_word
 
 
-for i in range(len(word)):
-    print(ord(word[i]))
-
-
-print()
-
-
-for i in range(len(word)):
-    zaszyfrowane = (ord(word[i]) + 1)
-
-    if zaszyfrowane > 122:
-        zaszyfrowane -= 26
-
-    print(chr(zaszyfrowane))
+hypothesis = Hypothesis4()
+print("Your encrypted word: ", hypothesis.encrypt("school"))
