d78a43aa84eececdaf19b69757181eeeb6573f9a
cos <uczen.ckziu@jaworzno.pl>
2022-12-09 12:24:10 +0100

    end

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ word = "kaszanka"
 for i in range(len(word)):
     print(ord(word[i]))
 
-#
-# letter = 'a'
-# print("Stara literka: ", letter)
-# letter_ascii = ord(letter)
-# letter_ascii += 3
-# letter = chr(letter_ascii)
-# print("Literka przesunięta i jeden: ", letter)
+
+print()
+
+
+for i in range(len(word)):
+    zaszyfrowane = (ord(word[i]) + 1)
+
+    if zaszyfrowane > 122:
+        zaszyfrowane -= 26
+
+    print(chr(zaszyfrowane))
